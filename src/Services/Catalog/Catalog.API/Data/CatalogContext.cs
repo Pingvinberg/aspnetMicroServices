9296e522e4a44dd14b54b4c807f2c6e51793e021
Pingvinberg <Pingvinberg@gmail.com>
2022-09-09 22:50:45 +0200

    Update catalog api

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {        
+        public CatalogContext(IOptions<ProductDatabaseSettings> productDatabaseSettings)
+        {
+            
+            var client = new MongoClient(productDatabaseSettings.Value.ConnectionString);
+            var database = client.GetDatabase(productDatabaseSettings.Value.DatabaseName);
+
+            Products = database.GetCollection<Product>(productDatabaseSettings.Value.CollectionName);
+            CatalogContextSeed.SeedData(Products);
+        }
+        
+        public IMongoCollection<Product> Products { get; }
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,11 +1,20 @@
+using Catalog.API.Data;
+using Catalog.API.Models;
+using Catalog.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<ProductDatabaseSettings>(builder.Configuration.GetSection("ProductDatabaseSettings"));
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 
 var app = builder.Build();
 
